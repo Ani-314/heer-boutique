08e30c2d86b37cc30b1c8a68fc8e2f42ad9bdd45
Ani-314 <aniketgiyar314@gmail.com>
2026-07-26 19:05:53 +0530

    script.js

--- script.js
+++ script.js
@@ -423,4 +423,14 @@ googleBtn.addEventListener("click",()=>{
     },300);
 
 });
+window.addEventListener("load", () => {
+    const loader = document.getElementById("loader");
 
+    if (loader) {
+        loader.style.opacity = "0";
+
+        setTimeout(() => {
+            loader.style.display = "none";
+        }, 500);
+    }
+});
